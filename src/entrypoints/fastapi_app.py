444cdea8c731863992804f3c48f2efb88495b68b
bogdan.mesesan <bogdan.mesesan@archive360.com>
2026-04-22 16:45:38 +0300

    Implement tdd decoupling of service layer from the domain layer models.

--- src/entrypoints/fastapi_app.py
+++ src/entrypoints/fastapi_app.py
@@ -1,3 +1,6 @@
+import datetime
+from typing import Optional
+
 import uvicorn
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
@@ -22,6 +25,15 @@ class AllocateResponse(BaseModel):
     batchref: str
 
 
+class AddBatchDescriptor(BaseModel):
+    ref: str
+    sku: str
+    qty: int
+    eta: Optional[str]
+
+class AddBatchResponse(BaseModel):
+    msg: str
+
 @app.post("/allocate", status_code=status.HTTP_201_CREATED, response_model=AllocateResponse)
 def allocate_endpoint(data: AllocateDescriptor):
     session = get_session()
@@ -31,7 +43,7 @@ def allocate_endpoint(data: AllocateDescriptor):
     )
 
     try:
-        batchref = services.allocate(line, repo, session)
+        batchref = services.allocate(data.orderid, data.sku, data.qty, repo, session)
     except (model.OutOfStock, services.InvalidSku) as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -40,4 +52,27 @@ def allocate_endpoint(data: AllocateDescriptor):
 
     return AllocateResponse(batchref=batchref)
 
-uvicorn.run(app, host="0.0.0.0", port=8000)
+@app.post("/add_batch", status_code=status.HTTP_201_CREATED, response_model=AddBatchResponse)
+def add_batch(data: AddBatchDescriptor):
+    session = get_session()
+    repo = repository.SqlAlchemyRepository(session)
+    eta = data.eta
+    if eta is not None:
+        eta = datetime.datetime.fromisoformat(eta).date()
+    services.add_batch(
+        data.ref,
+        data.sku,
+        data.qty,
+        eta,
+        repo,
+        session,
+    )
+    return AddBatchResponse(msg="Ok")
+
+
+def main():
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
--- src/service_layer/services.py
+++ src/service_layer/services.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from datetime import date
+from typing import Optional
+
 from src.adapters.repository import AbstractRepository
 from src.domain import model
 from src.domain.model import OrderLine
@@ -13,7 +16,19 @@ def is_valid_sku(sku, batches):
     return sku in {b.sku for b in batches}
 
 
-def allocate(line: OrderLine, repo: AbstractRepository, session) -> str:
+def add_batch(
+    ref: str, sku: str, qty: int, eta: Optional[date],
+    repo: AbstractRepository, session,
+) -> None:
+    repo.add(model.Batch(ref, sku, qty, eta))
+    session.commit()
+
+
+def allocate(
+    orderid: str, sku: str, qty: int,
+    repo: AbstractRepository, session
+) -> str:
+    line = OrderLine(orderid, sku, qty)
     batches = repo.list()
     if not is_valid_sku(line.sku, batches):
         raise InvalidSku(f"Invalid sku {line.sku}")
